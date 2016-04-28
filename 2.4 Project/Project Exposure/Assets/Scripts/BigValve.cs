fe3992b426c62666c5d2d47af2e0ba03405b51ea
VladislavVidenov <vladislavvidenov95@gmail.com>
2016-04-28 15:44:46 +0200

    update 0.06

---
+++ 2.4 Project/Project Exposure/Assets/Scripts/BigValve.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigValve : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
