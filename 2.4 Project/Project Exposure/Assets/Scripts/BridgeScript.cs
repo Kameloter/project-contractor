81e9b2e551ad16a1c4fc4bcd0c89969a5e6320cd
Kameloter <kameloter@hotmail.nl>
2016-04-28 15:42:37 +0200

    Update 0.05
    Added pickable objects

---
+++ 2.4 Project/Project Exposure/Assets/Scripts/BridgeScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeScript : Interactable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void Activate() {
+        print("hiiiiiiiiiiiii");
+    }
+
+    public override void Deactivate() {
+
+    }
+}
