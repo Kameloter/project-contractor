aa14f99338eb8389021b39f1c0508cfc81ba19dc
Kameloter <kameloter@hotmail.nl>
2016-04-28 14:23:54 +0200

    Update 0.04
    Mirror scripts and solar pane;

---
+++ 2.4 Project/Project Exposure/Assets/Scripts/MirrorScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MirrorScript : MonoBehaviour {
+
+    [HideInInspector]
+    public int state = 0;
+
+    Vector3 rotation = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    void OnMouseDown() {
+        if (state == 7) {
+            state = 0;
+        }
+        else {
+            state++;
+        }
+        print(state);
+
+        Vector3 rotation = new Vector3(0,state * 45,0);
+        this.transform.eulerAngles = rotation;
+
+    }
+}
