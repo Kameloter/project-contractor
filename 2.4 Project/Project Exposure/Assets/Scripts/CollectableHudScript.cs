8f4e3f6ac0006239cae956d8699f623ba4ca97f1
Martijn Snip <martijnsnip@gmail.com>
2016-05-24 13:01:29 +0200

    Update v0.60
    error checking + cleanup

--- 2.4 Project/Project Exposure/Assets/Scripts/CollectableHudScript.cs
+++ 2.4 Project/Project Exposure/Assets/Scripts/CollectableHudScript.cs
@@ -4,12 +4,15 @@ using System.Collections;
 
 public class CollectableHudScript : MonoBehaviour {
     [Header("Textfields")]
-    public Text divider;
-    public Text collectables;
-    public Text found;
+    //public Text divider;  
+    //public Text collectables;
+
+    public Text found; //will show the amount of collectables found this level
     public Text total; //will show the total amount of collectables in a level
 
     public void Start() {
+        if (referenceErrorCheck()) return; //invalid references
+
         UpdateHudFound();
         UpdateHudTotal();
     }
@@ -22,6 +25,7 @@ public class CollectableHudScript : MonoBehaviour {
 
     public void UpdateHudFound() {
         //The amount of collectables found this level is stored in the player script
+
         found.text = GameManager.Instance.PlayerScript.collectables.ToString();
     }
 
@@ -30,5 +34,16 @@ public class CollectableHudScript : MonoBehaviour {
         total.text = GameManager.Instance.SceneStats.CollectablesAvailable.ToString();
     }
 
-    
+    /// <summary>
+    /// Checks the references.
+    /// </summary>
+    /// <returns>True if errors encountered</returns>
+    public bool referenceErrorCheck() {
+        bool error = false;
+
+        if (found == null) { Debug.LogError("Collectable HUD: Cannot find 'found'-textfield."); error = true; }
+        if (total == null) { Debug.LogError("Collectable HUD: Cannot find 'total'-textfield."); error = true; }
+
+        return error;
+    }
 }
