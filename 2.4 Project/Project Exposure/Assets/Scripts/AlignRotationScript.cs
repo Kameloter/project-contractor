7a4563fb00a25a27260c50a1cc7e2d0192590ae3
Martijn Snip <martijnsnip@gmail.com>
2016-05-30 12:52:11 +0200

    Update 0.84b
    renamed script

---
+++ 2.4 Project/Project Exposure/Assets/Scripts/AlignRotationScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlignRotationScript : MonoBehaviour {
+
+    public Transform alignTo;  //camera?
+    void Start() {
+        if (alignTo != null) this.transform.rotation = alignTo.rotation;
+        else Debug.LogError("Assign something to the 'AlignTo' variable on '" + gameObject.name + "' from '" + gameObject.transform.parent.name + "'.");
+    }
+    
+    //When do we actually change te camera?
+    //void Update () {
+    //   this.transform.rotation = alignTo.rotation;
+    //}
+}
