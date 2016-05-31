1d0c98f213a83ba61b570b0a416ac3edac2fae3e
Martijn Snip <martijnsnip@gmail.com>
2016-05-31 14:49:19 +0200

    Update v0.95 - OutlineShader

---
+++ 2.4 Project/Project Exposure/Assets/ShaderOutlinerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShaderOutlinerScript : MonoBehaviour {
+
+
+    BaseInteractable owner;
+    MeshRenderer meshRenderer;
+    Material outlineMaterial;
+
+
+
+    public void Start() {
+        meshRenderer = GetComponent<MeshRenderer>();
+        outlineMaterial = Resources.Load("ObjectOutliner", typeof(Material)) as Material;
+        owner = GetComponent<BaseInteractable>();
+        owner.onTriggerEnterEvent.AddListener(AddOutlineMaterial);
+        owner.onTriggerExitEvent.AddListener(RemoveOutlineMaterial);
+    }
+
+    public void AddOutlineMaterial() {
+        Material original = meshRenderer.material; //store a copy of the material ON the object( shared material is THE MATERIAL INSTANCE => affects all object with that mat)
+
+        Material[] newMaterials = new Material[2]; //make a new array with materials
+        newMaterials[0] = original;
+        newMaterials[1] = outlineMaterial;
+
+        meshRenderer.sharedMaterials = newMaterials;
+    }
+
+    public void RemoveOutlineMaterial() {
+        Material original = meshRenderer.materials[0];
+        Material[] newMaterials = new Material[1];
+        newMaterials[0] = original;
+
+        meshRenderer.sharedMaterials = newMaterials;
+    }
+
+    void OnDestroy() {
+        owner.onTriggerEnterEvent.RemoveListener(AddOutlineMaterial);
+        owner.onTriggerExitEvent.RemoveListener(RemoveOutlineMaterial);
+    }
+}
