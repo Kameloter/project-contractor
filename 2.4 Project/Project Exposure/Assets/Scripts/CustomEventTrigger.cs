cb8ae60cc55367eb482c12f7309b9c9f2b6312d1
Kameloter <kameloter@hotmail.nl>
2016-06-06 14:53:57 +0200

    Update 1.40

--- 2.4 Project/Project Exposure/Assets/Scripts/CustomEventTrigger.cs
+++ 2.4 Project/Project Exposure/Assets/Scripts/CustomEventTrigger.cs
@@ -8,7 +8,7 @@ public class CustomEventTrigger : MonoBehaviour {
 
     [SerializeField]
     public enum Action {
-        PlaySound, PlayAnimation, PlayCameraPath, ActivateInteractable, DeactivateInteractable, ShowHint, PlayParticle, StopParticle, FocusOnTarget, ActivateLight, DisableLight, ChangeLightValues, ChangeImageEffects, ActivateObject, DeactivateObject
+        PlaySound, PlayAnimation, PlayCameraPath, ActivateInteractable, DeactivateInteractable, ShowHint, PlayParticle, StopParticle, FocusOnTarget, ActivateLight, DisableLight, ChangeLightValues, ChangeImageEffects, ActivateObject, DeactivateObject, ChangeCameraOffset
     };
 
     [SerializeField]
@@ -21,14 +21,18 @@ public class CustomEventTrigger : MonoBehaviour {
         Once, Delayed, Repeat, RepeatDelayed
     };
 
-    bool triggered = false;
-
     [System.Serializable]
     public struct info {
         public Action action;
 
         [SerializeField]
         public OnTrigger onTrigger;
+
+        [SerializeField]
+        public bool triggerMore;
+
+        [SerializeField]
+        public bool triggered;
 
         [SerializeField]
         public FireType fireType;
@@ -96,143 +100,127 @@ public class CustomEventTrigger : MonoBehaviour {
 
         [SerializeField]
         public float range;
+
+        [SerializeField]
+        public Vector3 offset;
     }
 
     [SerializeField]
     public info[] Go;
 
-    // Use this for initialization
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
-
     IEnumerator DoEvent(int i, float waitTime) {
         yield return new WaitForSeconds(waitTime);
 
-        switch (Go[i].action) {
-            case Action.ActivateInteractable:
-                Go[i].interactable.Activate();
-                break;
-            case Action.DeactivateInteractable:
-                Go[i].interactable.Deactivate();
-                break;
-            case Action.PlayAnimation:
-                Go[i].go.GetComponent<Animation>().clip = Go[i].animation;
-                Go[i].go.GetComponent<Animation>().Play();
-                break;
-            case Action.PlayCameraPath:
-                Camera.main.GetComponent<CameraControl>().StartCutscene(Go[i].path);
-                break;
-            case Action.PlaySound:
-                Go[i].go.GetComponent<AudioSource>().clip = Go[i].audioClip;
-                Go[i].go.GetComponent<AudioSource>().Play();
-                break;
-            case Action.ShowHint:
-                //gotyn
-                break;
-            case Action.ActivateLight:
-                Go[i].light.enabled = true;
-                break;
-            case Action.DisableLight:
-                Go[i].light.enabled = false;
-                break;
-            case Action.ChangeLightValues:
-                if (Go[i].light.range != 0) {
-                    Go[i].light.range = Go[i].range;
-                }
-                Go[i].light.color = Go[i].color;
-                Go[i].light.intensity = Go[i].intensity;
-                Go[i].light.bounceIntensity = Go[i].bounceIntensity;
-                break;
-            case Action.ActivateObject:
-                Go[i].go.SetActive(true);
-                break;
-            case Action.DeactivateObject:
-                Go[i].go.SetActive(false);
-                break;
-            case Action.PlayParticle:
-                Go[i].particle.Play();
-                break;
-            case Action.StopParticle:
-                Go[i].particle.Stop();
-                break;
-            case Action.FocusOnTarget:
-                Camera.main.GetComponent<CameraControl>().StartCutscene(Go[i].go);
-                break;
-        }
-    }
-
-    void OnTriggerEnter(Collider other) {
-        if (!triggered)
-        {
-            if (other.CompareTag(Tags.player))
-            {
-                for (int i = 0; i < Go.Length; i++)
-                {
-                    if (Go[i].onTrigger == OnTrigger.OnTriggerEnter)
-                    {
-                        switch (Go[i].fireType)
-                        {
-                            case FireType.Once:
-                                StartCoroutine(DoEvent(i, 0));
-                                break;
-                            case FireType.Delayed:
-                                StartCoroutine(DoEvent(i, Go[i].delay));
-                                break;
-                            case FireType.Repeat:
-                                for (int j = 0; j < Go[i].repeatAmount; j++)
-                                {
-                                    StartCoroutine(DoEvent(i, 0 + j * Go[i].repeatTime));
-                                }
-                                break;
-                            case FireType.RepeatDelayed:
-                                for (int j = 0; j < Go[i].repeatAmount; j++)
-                                {
-                                    StartCoroutine(DoEvent(i, Go[i].delay + j * Go[i].repeatTime));
-                                }
-                                break;
-                        }
+        if (!Go[i].triggered) {
+            switch (Go[i].action) {
+                case Action.ActivateInteractable:
+                    Go[i].interactable.Activate();
+                    break;
+                case Action.DeactivateInteractable:
+                    Go[i].interactable.Deactivate();
+                    break;
+                case Action.PlayAnimation:
+                    Go[i].go.GetComponent<Animation>().clip = Go[i].animation;
+                    Go[i].go.GetComponent<Animation>().Play();
+                    break;
+                case Action.PlayCameraPath:
+                    Camera.main.GetComponent<CameraControl>().StartCutscene(Go[i].path);
+                    break;
+                case Action.PlaySound:
+                    Go[i].go.GetComponent<AudioSource>().clip = Go[i].audioClip;
+                    Go[i].go.GetComponent<AudioSource>().Play();
+                    break;
+                case Action.ShowHint:
+                    //gotyn
+                    break;
+                case Action.ActivateLight:
+                    Go[i].light.enabled = true;
+                    break;
+                case Action.DisableLight:
+                    Go[i].light.enabled = false;
+                    break;
+                case Action.ChangeLightValues:
+                    if (Go[i].light.range != 0) {
+                        Go[i].light.range = Go[i].range;
                     }
-                }
+                    Go[i].light.color = Go[i].color;
+                    Go[i].light.intensity = Go[i].intensity;
+                    Go[i].light.bounceIntensity = Go[i].bounceIntensity;
+                    break;
+                case Action.ActivateObject:
+                    Go[i].go.SetActive(true);
+                    break;
+                case Action.DeactivateObject:
+                    Go[i].go.SetActive(false);
+                    break;
+                case Action.PlayParticle:
+                    Go[i].particle.Play();
+                    break;
+                case Action.StopParticle:
+                    Go[i].particle.Stop();
+                    break;
+                case Action.FocusOnTarget:
+                    Camera.main.GetComponent<CameraControl>().StartCutscene(Go[i].go);
+                    break;
+                case Action.ChangeCameraOffset:
+                    Camera.main.GetComponent<CameraControl>().offset = Go[i].offset;
+                    break;
+            }
+            if (!Go[i].triggerMore) {
+                Go[i].triggered = true;
             }
         }
     }
 
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(Tags.player)) {
+            for (int i = 0; i < Go.Length; i++) {
+                if (Go[i].onTrigger == OnTrigger.OnTriggerEnter) {
+                    switch (Go[i].fireType) {
+                        case FireType.Once:
+                            StartCoroutine(DoEvent(i, 0));
+                            break;
+                        case FireType.Delayed:
+                            StartCoroutine(DoEvent(i, Go[i].delay));
+                            break;
+                        case FireType.Repeat:
+                            for (int j = 0; j < Go[i].repeatAmount; j++) {
+                                StartCoroutine(DoEvent(i, 0 + j * Go[i].repeatTime));
+                            }
+                            break;
+                        case FireType.RepeatDelayed:
+                            for (int j = 0; j < Go[i].repeatAmount; j++) {
+                                StartCoroutine(DoEvent(i, Go[i].delay + j * Go[i].repeatTime));
+                            }
+                            break;
+                    }
+                }
+            }
+
+        }
+    }
+
     void OnTriggerStay(Collider other) {
-        if (!triggered)
-        {
-            if (other.CompareTag(Tags.player))
-            {
-                for (int i = 0; i < Go.Length; i++)
-                {
-                    if (Go[i].onTrigger == OnTrigger.OnTriggerStay)
-                    {
-                        switch (Go[i].fireType)
-                        {
-                            case FireType.Once:
-                                StartCoroutine(DoEvent(i, 0));
-                                break;
-                            case FireType.Delayed:
-                                StartCoroutine(DoEvent(i, Go[i].delay));
-                                break;
-                            case FireType.Repeat:
-                                for (int j = 0; j < Go[i].repeatAmount; j++)
-                                {
-                                    StartCoroutine(DoEvent(i, 0 + j * Go[i].repeatTime));
-                                }
-                                break;
-                            case FireType.RepeatDelayed:
-                                for (int j = 0; j < Go[i].repeatAmount; j++)
-                                {
-                                    StartCoroutine(DoEvent(i, Go[i].delay + j * Go[i].repeatTime));
-                                }
-                                break;
-                        }
+        if (other.CompareTag(Tags.player)) {
+            for (int i = 0; i < Go.Length; i++) {
+                if (Go[i].onTrigger == OnTrigger.OnTriggerStay) {
+                    switch (Go[i].fireType) {
+                        case FireType.Once:
+                            StartCoroutine(DoEvent(i, 0));
+                            break;
+                        case FireType.Delayed:
+                            StartCoroutine(DoEvent(i, Go[i].delay));
+                            break;
+                        case FireType.Repeat:
+                            for (int j = 0; j < Go[i].repeatAmount; j++) {
+                                StartCoroutine(DoEvent(i, 0 + j * Go[i].repeatTime));
+                            }
+                            break;
+                        case FireType.RepeatDelayed:
+                            for (int j = 0; j < Go[i].repeatAmount; j++) {
+                                StartCoroutine(DoEvent(i, Go[i].delay + j * Go[i].repeatTime));
+                            }
+                            break;
                     }
                 }
             }
@@ -240,40 +228,30 @@ public class CustomEventTrigger : MonoBehaviour {
     }
 
     void OnTriggerExit(Collider other) {
-        if (!triggered)
-        {
-            if (other.CompareTag(Tags.player))
-            {
-                for (int i = 0; i < Go.Length; i++)
-                {
-                    if (Go[i].onTrigger == OnTrigger.OnTriggerExit)
-                    {
-                        switch (Go[i].fireType)
-                        {
-                            case FireType.Once:
-                                StartCoroutine(DoEvent(i, 0));
-                                break;
-                            case FireType.Delayed:
-                                StartCoroutine(DoEvent(i, Go[i].delay));
-                                break;
-                            case FireType.Repeat:
-                                for (int j = 0; j < Go[i].repeatAmount; j++)
-                                {
-                                    StartCoroutine(DoEvent(i, 0 + j * Go[i].repeatTime));
-                                }
-                                break;
-                            case FireType.RepeatDelayed:
-                                for (int j = 0; j < Go[i].repeatAmount; j++)
-                                {
-                                    StartCoroutine(DoEvent(i, Go[i].delay + j * Go[i].repeatTime));
-                                }
-                                break;
-                        }
+        if (other.CompareTag(Tags.player)) {
+            for (int i = 0; i < Go.Length; i++) {
+                if (Go[i].onTrigger == OnTrigger.OnTriggerExit) {
+                    switch (Go[i].fireType) {
+                        case FireType.Once:
+                            StartCoroutine(DoEvent(i, 0));
+                            break;
+                        case FireType.Delayed:
+                            StartCoroutine(DoEvent(i, Go[i].delay));
+                            break;
+                        case FireType.Repeat:
+                            for (int j = 0; j < Go[i].repeatAmount; j++) {
+                                StartCoroutine(DoEvent(i, 0 + j * Go[i].repeatTime));
+                            }
+                            break;
+                        case FireType.RepeatDelayed:
+                            for (int j = 0; j < Go[i].repeatAmount; j++) {
+                                StartCoroutine(DoEvent(i, Go[i].delay + j * Go[i].repeatTime));
+                            }
+                            break;
                     }
                 }
             }
         }
-        triggered = true;
     }
 }
 
