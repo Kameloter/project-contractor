57473d21afeabd899efc41725073ab788f644afb
Kameloter <kameloter@hotmail.nl>
2016-06-08 15:56:38 +0200

    Update 1.61
    some bugs fixed

---
+++ 2.4 Project/Project Exposure/Assets/ValveRespawnScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ValveRespawnScript : MonoBehaviour {
+  
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(Tags.valve)) {
+            other.GetComponent<PickableScript>().ResetPos();
+        }
+    }
+}
