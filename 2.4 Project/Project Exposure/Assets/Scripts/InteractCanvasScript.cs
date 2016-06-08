cf7b65c3876a469e5ca16e3c243e3f96a3896596
Martijn Snip <martijnsnip@gmail.com>
2016-06-08 15:50:03 +0200

    1

--- 2.4 Project/Project Exposure/Assets/Scripts/InteractCanvasScript.cs
+++ 2.4 Project/Project Exposure/Assets/Scripts/InteractCanvasScript.cs
@@ -2,68 +2,46 @@
 using UnityEngine.UI;
 using System.Collections;
 
+/// <summary>
+/// Adds listeners to enable / disable the ObjectCanvas.
+/// </summary>
 [RequireComponent(typeof (AlignRotationScript))]
 public class InteractCanvasScript : MonoBehaviour {
-    public enum ImageType { ValvePlacement, ValveRotate, Other }
-
-    BaseInteractable owner; //The object that has 
+    BaseInteractable owner;
     AlignRotationScript alignScript;
 
     [Header("References (auto)")]
     public Canvas objectCanvas;
     public Image imageObject;
     
-
-    [Header("Interaction Image Type")]
-    public ImageType imageType;
-    [ReadOnly] public Sprite interactionSprite;
-
-    SpriteState spriteState = new SpriteState();
-    Button button;
-
     void Start() {
-        owner = transform.root.GetComponent<BaseInteractable>();
-        if (owner == null) owner = transform.root.GetComponentInChildren<BaseInteractable>();
-        if (owner == null) Debug.LogError("Cannot find owner.");
-
-        objectCanvas = GetComponentInChildren<Canvas>();
-        if (objectCanvas != null) objectCanvas.enabled = false;
-        else Debug.LogError("Assign something to the 'objectCanvas' variable on '" + gameObject.name + "'.");
+        FindOwner();
+        FindObjectCanvas();
+        MakeSpriteInvisible();
 
         alignScript = GetComponent<AlignRotationScript>();
-
-        imageObject = GetComponentInChildren<Image>();
-        button = imageObject.gameObject.GetComponent<Button>();
-
-        if      (imageType == ImageType.ValvePlacement) {
-            //set the button transition to spriteswap
-            button.transition = Selectable.Transition.SpriteSwap;
-
-            //set the proper sprites
-            spriteState.highlightedSprite = Resources.Load("ValveSprite_Pressed", typeof(Sprite)) as Sprite;
-            spriteState.pressedSprite = Resources.Load("ValveSprite_Pressed", typeof(Sprite)) as Sprite;
-            button.spriteState = spriteState;
-
-            //set the normal sprite
-            interactionSprite = Resources.Load("ValveSprite", typeof(Sprite)) as Sprite;
-        }
-        else if (imageType == ImageType.ValveRotate   ) {
-            button.transition = Selectable.Transition.ColorTint;
-            ColorBlock colorBlock = new ColorBlock();
-            colorBlock.normalColor = Color.white;
-            colorBlock.highlightedColor = Color.red;
-            colorBlock.pressedColor = Color.red;
-            colorBlock.colorMultiplier = 1;
-            button.colors = colorBlock;
-            interactionSprite = Resources.Load("RotateSprite", typeof(Sprite)) as Sprite;
-        }
-        else if (imageType == ImageType.Other         ) { /*Image should be specified in inspector*/ }
-        imageObject.sprite = interactionSprite;
 
         owner.onTriggerEnterEvent.AddListener(ActivateCanvas);
         owner.onTriggerExitEvent.AddListener(DeactivateCanvas);
     }
 
+    void FindOwner() {
+        owner = transform.parent.GetComponent<BaseInteractable>(); //Try to get the owner from the parent
+        if (owner == null) owner = transform.root.GetComponentInChildren<BaseInteractable>(); //if previous failed, try to get owner from any of the parents children
+        if (owner == null) Debug.LogError("Cannot find owner.");
+    }
+
+    void FindObjectCanvas() {
+        objectCanvas = GetComponent<Canvas>();
+        if (objectCanvas != null) objectCanvas.enabled = false;
+        else Debug.LogError("Assign something to the 'objectCanvas' variable on '" + gameObject.name + "'.");
+    }
+
+    void MakeSpriteInvisible() {
+        imageObject = GetComponentInChildren<Image>();
+        imageObject.color = new Color(1, 1, 1, 0); 
+    }
+    
     void ActivateCanvas() {
         objectCanvas.enabled = true;
         alignScript.Align();
