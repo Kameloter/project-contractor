f33a716f1f352a657f9382eb0d3c3f41d5c0293c
Martijn Snip <martijnsnip@gmail.com>
2016-06-09 15:31:35 +0200

    Update 1.69 - tutorialselector

---
+++ 2.4 Project/Project Exposure/Assets/TutorialSelectorScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialSelectorScript : MonoBehaviour {
+    [Header("Reference (optional)")]
+    [SerializeField] Animator tutorialAnimator;
+
+    GameObject repeatButton;
+
+    void Awake () {
+        if (tutorialAnimator == null) tutorialAnimator = GameObject.Find("Tutorial").GetComponent<Animator>();
+        repeatButton = GameObject.Find("Repeat_Button");
+        Exit();
+    }
+    
+    public void ShowTutorial(string tutorialName) {
+        tutorialAnimator.SetTrigger(tutorialName);
+        repeatButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        repeatButton.GetComponent<Button>().onClick.AddListener(() => { tutorialAnimator.SetTrigger(tutorialName); });
+        Exit();
+    }	
+
+    public void Exit() {
+        gameObject.SetActive(false);
+    }
+
+    public void Enable() {
+        if (tutorialAnimator.GetComponent<Canvas>().enabled) tutorialAnimator.SetTrigger("Exit");
+        gameObject.SetActive(true);
+    }
+}
