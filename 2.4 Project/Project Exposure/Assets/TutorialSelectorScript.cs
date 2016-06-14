bf716a53ce47495ddbfefde7e751a6d83acd846e
Martijn Snip <martijnsnip@gmail.com>
2016-06-14 15:05:20 +0200

    Update 1.98

--- 2.4 Project/Project Exposure/Assets/TutorialSelectorScript.cs
+++ 2.4 Project/Project Exposure/Assets/TutorialSelectorScript.cs
@@ -5,33 +5,55 @@ using System.Collections;
 public class TutorialSelectorScript : MonoBehaviour {
     [Header("Reference (optional)")]
     [SerializeField] Animator tutorialAnimator;
+    [SerializeField] Animator monitorAnimator;
 
     GameObject repeatButton;
 
     void Awake () {
         if (tutorialAnimator == null) tutorialAnimator = GameObject.Find("Tutorial").GetComponent<Animator>();
+        if (monitorAnimator == null) monitorAnimator = GameObject.Find("MonitorObj").GetComponent<Animator>();
         repeatButton = GameObject.Find("Repeat_Button");
-        Exit();
+        CloseSelector();
     }
     
     public void ShowTutorial(string tutorialName) {
         tutorialAnimator.SetTrigger(tutorialName);
         repeatButton.GetComponent<Button>().onClick.RemoveAllListeners();
         repeatButton.GetComponent<Button>().onClick.AddListener(() => { tutorialAnimator.SetTrigger(tutorialName); });
-        Exit();
+        CloseSelector();
     }	
 
-    public void Exit() {
-        gameObject.SetActive(false);
+    public void CloseSelector() {
+        if (monitorAnimator.GetCurrentAnimatorStateInfo(0).IsName("Up")) monitorAnimator.SetTrigger("Down");
+        DisableSelector();
     }
 
-    public void Enable() {
+    public void OpenSelector() {
+        if (monitorAnimator.GetCurrentAnimatorStateInfo(0).IsName("Down")) monitorAnimator.SetTrigger("Up");
+        
+        //if any tutorial is showing right now, disable it.
         if (tutorialAnimator.GetComponent<Canvas>().enabled) tutorialAnimator.SetTrigger("Exit");
-        gameObject.SetActive(true);
+        Invoke("EnableSelector", 0.3f); //magic value
+    }
+
+    void MonitorUp() {
+        monitorAnimator.SetTrigger("Up");
+    }
+
+    void MonitorDown() {
+        monitorAnimator.SetTrigger("Down");
     }
 
     public void Toggle() {  //changes state
-        if (gameObject.activeSelf) Exit();
-        else Enable();
+        if (gameObject.activeSelf) CloseSelector();
+        else OpenSelector();
+    }
+
+    void EnableSelector() {
+        gameObject.SetActive(true);
+    }
+
+    void DisableSelector() {
+        gameObject.SetActive(false);
     }
 }
