fc3ba19fea2b786fcd9f3dd8d5828c6fb22b427f
Martijn Snip <martijnsnip@gmail.com>
2016-06-15 11:11:34 +0200

    Update v2.10

---
+++ 2.4 Project/Project Exposure/Assets/MonitorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script is used to automatically set the Main Camera as Render Camera for the 
+/// canvas the monitor is on, if there is none specified.
+/// </summary>
+public class MonitorScript : MonoBehaviour {
+     Canvas canvas;
+
+    void Awake() {
+        canvas = GetComponent<Canvas>();
+        if (canvas.worldCamera == null) canvas.worldCamera = Camera.main;
+    }
+}
