fb2d831aa8543e6593db8d59b66505d2454da64d
Martijn Snip <martijnsnip@gmail.com>
2016-06-20 16:27:12 +0200

    1

--- 2.4 Project/Project Exposure/Assets/ScoreScreenScript.cs
+++ 2.4 Project/Project Exposure/Assets/ScoreScreenScript.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class ScoreScreenScript : MonoBehaviour {
     //scorescreen
     GameObject scoreScreen;
-    Button continueBtn, stayBtn;
-    Text timeSpentText, timeLeftText, collectedText;
+    [SerializeField] Button continueBtn, stayBtn;
+    [SerializeField] Text timeSpentText, timeLeftText, collectedText;
+    [SerializeField] Animator star_1, star_2, star_3;
 
     void Start() {
+        scoreScreen = transform.gameObject;
+        
         SetupScoreScreenElements();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.U)) AddStar(1);
+        if (Input.GetKeyDown(KeyCode.I)) AddStar(2);
+        if (Input.GetKeyDown(KeyCode.O)) AddStar(3);
+        if (Input.GetKeyDown(KeyCode.P)) AddStar(4);
     }
 
     /// <summary>
     /// Setups ScoreScreen elements and disables the scorescreen.
     /// </summary>
     void SetupScoreScreenElements() {
-        scoreScreen = GameObject.Find("ScoreScreen");
-        timeSpentText = GameObject.Find("TimeSpentAmount").GetComponent<Text>();
-        collectedText = GameObject.Find("CollectablesAmount").GetComponent<Text>();
-        timeLeftText = GameObject.Find("RemainingTimeAmount").GetComponent<Text>();
-
-        continueBtn = scoreScreen.transform.FindChild("ContinueButton").GetComponent<Button>();
-        stayBtn = scoreScreen.transform.FindChild("StayButton").GetComponent<Button>();
+        if (NullCheck()) return; 
 
         continueBtn.onClick.RemoveAllListeners();
         continueBtn.onClick.AddListener(() => { Continue(); });
@@ -31,6 +36,18 @@ public class ScoreScreenScript : MonoBehaviour {
         stayBtn.onClick.AddListener(() => { Stay(); });
 
         scoreScreen.SetActive(false);
+    }
+
+    /// <summary>
+    /// Checks if any reference is null. Returns true if any is.
+    /// </summary>
+    /// <returns></returns>
+    bool NullCheck() {
+        if (!continueBtn || !stayBtn || !timeSpentText || !timeLeftText || !collectedText || !star_1 || !star_2 || !star_3) {
+            Debug.LogError("At least one of the ScoreScreen references is missing. Please check the ScoreScreen gameObject.", transform);
+            Debug.Break();
+            return true;
+        } else return false;
     }
 
     /// <summary>
@@ -77,5 +94,23 @@ public class ScoreScreenScript : MonoBehaviour {
     /// </summary>
     void Stay() { //Called by button on scorescreen.
         DisableScoreScreen();
+    }
+
+    void AddStar(int starNumber) {
+        string trigger = "GiveStar";
+        switch (starNumber) {
+            case 3:
+                star_3.SetTrigger(trigger);
+                goto case 2;
+            case 2:
+                star_2.SetTrigger(trigger);
+                goto case 1;
+            case 1:
+                star_1.SetTrigger(trigger);
+                break;
+            default:
+                Debug.LogError("AddStar: Insert a valid number (1-3).", transform);
+                break;
+        }
     }
 }
