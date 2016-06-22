43c56db5374bc9ede568f5fae6d5f87b77ae4f75
VladislavVidenov <vladislavvidenov95@gmail.com>
2016-06-22 23:41:24 +0200

    Update v3.00

--- 2.4 Project/Project Exposure/Assets/AnimatedDoor.cs
+++ 2.4 Project/Project Exposure/Assets/AnimatedDoor.cs
@@ -1,40 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AnimatedDoor : BaseActivatable
+public class AnimatedDoor : MonoBehaviour
 {
-    Animator myAnimator;
+   
 	// Use this for initialization
-	public override void Start () {
-        base.Start();
-        myAnimator = GetComponent<Animator>();
+	public void Start () {
+        GetComponent<Animator>().SetTrigger("Open");
+        GetComponentInChildren<BlinkRedLightControl>().StartBlinking();
 
 	}
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.G))
-        {
-            Activate();
-
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-
-            Deactivate();
-        }
-    }
-    public override void Activate()
-    {
-        myAnimator.speed = 1;
-        myAnimator.SetTrigger("Open");
-        base.Activate();
-    }
-
-    public override void Deactivate()
-    {
-        myAnimator.speed = -1;
-        myAnimator.Play("Close");
-        base.Deactivate();
-    }
+    
 }
